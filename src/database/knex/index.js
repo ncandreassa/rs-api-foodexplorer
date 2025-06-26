ceacc8bda6ae7675b8f540016b3bcba616fabe86
Nathalia <ncandreassa@hotmail.com>
2025-06-26 21:10:22 +0100

    add production knex config

--- src/database/knex/index.js
+++ src/database/knex/index.js
@@ -1,6 +1,7 @@
 const config = require("../../../knexfile");
 const knex = require("knex");
 
-const connection = knex(config.development);
+const envConfig = process.env.RENDER_ENV ? config[process.env.RENDER_ENV] : config.development
+const connection = knex(envConfig);
 
 module.exports = connection;

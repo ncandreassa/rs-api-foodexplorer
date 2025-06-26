06fc070381e8b6463d428582ae6afe8e988be913
Nathalia <ncandreassa@hotmail.com>
2025-06-26 20:42:27 +0100

    catch user create errors

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -6,27 +6,31 @@ const sqliteConnection = require("../database/sqlite");
 
 class UsersController {
   async create(request, response) {
-    const { name, email, password } = request.body;
+    try {
+      const { name, email, password } = request.body;
 
-    const database = await sqliteConnection();
-    const checkUserExist = await database.get(
-      "SELECT * FROM users WHERE email = (?)",
-      [email]
-    );
+      const database = await sqliteConnection();
+      const checkUserExist = await database.get(
+        "SELECT * FROM users WHERE email = (?)",
+        [email]
+      );
 
-    if (checkUserExist) {
-      throw new AppError("Este e-mail já está em uso!");
+      if (checkUserExist) {
+        throw new AppError("Este e-mail já está em uso!");
+      }
+
+      const hashedPassword = await hash(password, 8);
+      const role = email.includes("admin") ? "admin" : "user";
+
+      await database.run(
+        "INSERT INTO users (name, email, password, role) VALUES (?, ?, ?, ?)",
+        [name, email, hashedPassword, role]
+      );
+
+      return response.status(201).json();
+    } catch (error) {
+      console.error(error);
     }
-
-    const hashedPassword = await hash(password, 8);
-    const role = email.includes("admin") ? "admin" : "user"
-
-    await database.run(
-      "INSERT INTO users (name, email, password, role) VALUES (?, ?, ?, ?)",
-      [name, email, hashedPassword, role]
-    );
-
-    return response.status(201).json();
   }
 }
 

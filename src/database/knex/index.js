74c4000e4ea8eb2d185b902a10fbb304a3b3f41f
Nathalia <ncandreassa@hotmail.com>
2025-06-28 14:48:24 +0100

    log .env and knex config

--- src/database/knex/index.js
+++ src/database/knex/index.js
@@ -1,6 +1,9 @@
 const config = require("../../../knexfile");
 const knex = require("knex");
 
+console.log("process.env.RENDER_ENV", process.env.RENDER_ENV)
+console.log(config)
+
 const envConfig = process.env.RENDER_ENV ? config[process.env.RENDER_ENV] : config.development
 const connection = knex(envConfig);
 

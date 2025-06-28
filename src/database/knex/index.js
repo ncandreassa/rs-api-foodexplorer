7382b11e78d0ce3b746e4dbf237b087ebf5451e4
Nathalia <ncandreassa@hotmail.com>
2025-06-28 14:57:05 +0100

    use node config on knex

--- src/database/knex/index.js
+++ src/database/knex/index.js
@@ -1,6 +1,10 @@
 const config = require("../../../knexfile");
 const knex = require("knex");
 
-const connection = knex(config.development);
+console.log("process.env.RENDER_ENV", process.env.RENDER_ENV)
+console.log(config)
+
+const envConfig = process.env.RENDER_ENV ? config[process.env.RENDER_ENV] : config.development
+const connection = knex(envConfig);
 
 module.exports = connection;
